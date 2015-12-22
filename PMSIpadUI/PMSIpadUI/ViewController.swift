67df1c22351fb554265035fa1441b7232744c7b0
赵鑫 <ilyzhaoxin@sina.com>
2015-12-22 10:02:24 +0800

    新建项目

---
+++ PMSIpadUI/PMSIpadUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PMSIpadUI
+//
+//  Created by 赵鑫 on 15/12/22.
+//  Copyright © 2015年 赵鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
